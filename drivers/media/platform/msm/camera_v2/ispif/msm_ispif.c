d00eae0b1b116ee1c2bb6aec84e4bde3496ce487
skinzor <robertpopescu95@yahoo.com>
2017-12-30 20:02:59 +0200

    drivers/media: import Huawei changes

--- drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
+++ drivers/media/platform/msm/camera_v2/ispif/msm_ispif.c
@@ -1051,6 +1051,7 @@ static int msm_ispif_stop_immediately(struct ispif_device *ispif,
 			cid_mask, params->entries[i].vfe_intf, 0);
 	}
 
+	rc = msm_ispif_reset_hw(ispif);
 	return rc;
 }
 
@@ -1177,6 +1178,7 @@ static int msm_ispif_stop_frame_boundary(struct ispif_device *ispif,
 	}
 
 end:
+	rc = msm_ispif_reset_hw(ispif);
 	return rc;
 }
 
